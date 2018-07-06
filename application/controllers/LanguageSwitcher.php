38dd025cd9df1cd4f456af43862967579c640f6a
Saidur Rohman <37407965+saidhstu@users.noreply.github.com>
2018-07-06 17:43:07 +0600

    Update LanguageSwitcher.php

--- application/controllers/LanguageSwitcher.php
+++ application/controllers/LanguageSwitcher.php
@@ -1,7 +1,7 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 class LanguageSwitcher extends CI_Controller
 {
-    public function __construct() {
+    public function __construct($mark=0) {
         parent::__construct();     
     }
  
